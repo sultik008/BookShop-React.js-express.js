7813ac4c52c93f6e378ec92e7c172e2b73e25f81
Sultanbek <sultanbekzumamuratov171@gmail.com>
2025-08-07 03:07:22 +0500

    route edit front

--- controllers/controller.js
+++ controllers/controller.js
@@ -3,7 +3,8 @@ import { generateBooks } from '../services/service.js'
 export async function getBooks(req , res) {
     try {
         const { seed , language , likes , reviews , booksCount } = req.query
-        console.log({ seed , language , likes , reviews , booksCount })
+        console.log({ seed , language , likes , reviews , booksCount } , Date.now())
+        console.log(Date.now())
         const result = await generateBooks({ seed , language , likes , reviews , booksCount })
         res.json(result)
     } catch (error) {
